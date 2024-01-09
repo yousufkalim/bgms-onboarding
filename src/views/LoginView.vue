b4d41b5746873fe47e9137d6fc9037842fa71766
Yousuf Kalim <yousuf.kalim@xgrid.co>
2024-01-09 22:07:20 +0500

    Setup static login screen

---
+++ src/views/LoginView.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+        <div class="card border-0 shadow rounded-3 my-5">
+          <div class="card-body p-4 p-sm-5">
+            <h5 class="card-title text-center mb-5 fw-light fs-5">Sign In</h5>
+            <form @submit.prevent="handleSubmit">
+              <div class="form-floating mb-3">
+                <input
+                  type="email"
+                  class="form-control"
+                  id="floatingInput"
+                  placeholder="name@example.com"
+                  v-model="email"
+                />
+                <label for="floatingInput">Email address</label>
+              </div>
+              <div class="form-floating mb-3">
+                <input
+                  type="password"
+                  class="form-control"
+                  id="floatingPassword"
+                  placeholder="Password"
+                  v-model="password"
+                />
+                <label for="floatingPassword">Password</label>
+              </div>
+
+              <div class="form-check mb-3 d-flex justify-content-start">
+                <input
+                  class="form-check-input"
+                  type="checkbox"
+                  value=""
+                  id="rememberPasswordCheck"
+                  v-model="remember"
+                />
+                <label
+                  class="form-check-label mx-3"
+                  for="rememberPasswordCheck"
+                >
+                  Remember password
+                </label>
+              </div>
+              <div class="d-grid">
+                <button
+                  class="btn btn-primary btn-login text-uppercase fw-bold"
+                  type="submit"
+                >
+                  Sign in
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginView",
+  data() {
+    return {
+      email: "",
+      password: "",
+      remember: false,
+    };
+  },
+  methods: {
+    handleSubmit() {
+      console.log(this.email, this.password, this.remember);
+    },
+  },
+};
+</script>
+
+<style>
+.btn-login {
+  font-size: 0.9rem;
+  letter-spacing: 0.05rem;
+  padding: 0.75rem 1rem;
+}
+</style>
